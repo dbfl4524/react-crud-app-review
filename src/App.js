f4d3ade43f659fb9de0a02b03342a931c12a7f8d
dbfl4524 <sinyuri0525@gmail.com>
2024-02-27 01:40:12 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
 import { Component } from "react";
 import "./App.css";
+import ExpenseForm from "./components/ExpenseForm";
+import ExpenseList from "./components/ExpenseList";
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      expenses: [
+        { id: 1, charge: "아이폰 15 pro", amount: 1500000 },
+        { id: 2, charge: "맥북", amount: 3700000 },
+        { id: 3, charge: "에어팟", amount: 350000 },
+      ],
+    };
+  }
+
+  handleDelete = (id) => {
+    const newExpense = this.state.expenses.filter(
+      (expense) => expense.id !== id
+    );
+
+    this.setState({ expenses: newExpense });
+
+    console.log(newExpense);
+  };
+
   render() {
     return (
       <main className="main-container">
@@ -9,13 +32,18 @@ export default class App extends Component {
           <div
             style={{ width: "100%", backgroundColor: "white", padding: "1rem" }}
           >
-            {/* Expense Form */} Expense Form
+            {/* Expense Form */}
+            <ExpenseForm />
           </div>
 
           <div
             style={{ width: "100%", backgroundColor: "white", padding: "1rem" }}
           >
-            {/* Expense List */} Expense List
+            {/* Expense List */}
+            <ExpenseList
+              initialExpenses={this.state.expenses}
+              handleDelete={this.handleDelete}
+            />
           </div>
 
           <div
